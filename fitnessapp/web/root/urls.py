e220d0e997259c70a9c3d250f0f34712d6323b0d
yuliasukach <yuliyasukach123@gmail.com>
2023-04-18 16:36:41 +0200

    added create user through the form

--- fitnessapp/web/root/urls.py
+++ fitnessapp/web/root/urls.py
@@ -3,4 +3,6 @@ from fitnessapp.settings import API_V
 
 
 Controller.include('', 'fitnessapp.web.home.urls')
+Controller.include('', 'fitnessapp.web.user.urls')
 Controller.include(f'/api/v_{API_V}', 'fitnessapp.api.root.urls')
+
