af6377486554fefa2f7a40c48be82d4ecd1042e3
JuliaSukach <juliasukach17@gmail.com>
2024-02-27 10:59:10 +0100

    port update

--- fitnessapp/app/__main__.py
+++ fitnessapp/app/__main__.py
@@ -1,8 +1,9 @@
 from aiohttp import web
 from .srv import create_app
+import os
 
 try:
-    web.run_app(create_app(), host='localhost', port=8000)
+    web.run_app(create_app(), host='0.0.0.0', port=os.getenv('PORT'))
 except KeyboardInterrupt:
     print('\rStop Fitness App')
 
